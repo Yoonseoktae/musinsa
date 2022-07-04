28f23887dd31d89db2746122a63d83cfed18a204
Yoonseoktae <seknman123@gmail.com>
2022-07-04 14:57:10 +0000

    상품API 라우트

---
+++ app/Controllers/Api/Goods.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use CodeIgniter\Controller;
+
+class Goods extends Controller {
+
+    public function __construct() 
+    {
+
+    }
+
+    protected function get($id = false) 
+    {
+        
+    }
+
+    protected function post() 
+    {
+
+    }
+
+    protected function put($id = false) 
+    {
+
+    }
+
+    protected function delete($id = false) 
+    {
+
+    }
+
+    protected function patch($id = false)
+    {
+
+    }
+
+}
